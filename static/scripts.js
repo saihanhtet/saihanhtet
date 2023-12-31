6fa1e0bff16b11cdbe0ff0afb8af2f99fed8903f
saihanhtet <sai.hanhtetsan@gmail.com>
2023-12-31 14:38:12 +0700

    add vercel setup and add the navbar

--- static/scripts.js
+++ static/scripts.js
@@ -1,65 +1,88 @@
-document.addEventListener("DOMContentLoaded", function () {
-  function setTheme(themeName) {
-    localStorage.setItem("theme", themeName);
-    document.documentElement.className = themeName;
+function setTheme(themeName) {
+  localStorage.setItem("theme", themeName);
+  document.documentElement.className = themeName;
+  toggleThemeIcon();
+}
+
+// Function to toggle between light and dark themes
+function toggleTheme() {
+  if (localStorage.getItem("theme") === "theme-dark") {
+    setTheme("theme-light");
+  } else {
+    setTheme("theme-dark");
+  }
+}
+
+function toggleThemeIcon() {
+  const button = document.querySelector(".theme-toggler");
+  const sun = "bi bi-brightness-high-fill";
+  const moon = "bi bi-moon-fill";
+  let icon = "";
+
+  if (localStorage.getItem("theme") === "theme-dark") {
+    icon = `<i class="${sun}"></i>`;
+  } else {
+    icon = `<i class="${moon}"></i>`;
   }
 
-  // Function to toggle between light and dark themes
-  function toggleTheme() {
-    if (localStorage.getItem("theme") === "theme-dark") {
-      setTheme("theme-light");
-    } else {
-      setTheme("theme-dark");
+  button.innerHTML = icon;
+}
+// function that create the snow flakes
+function createSnowflakes() {
+  const matrixContainer = document.querySelector(".matrix-container");
+  const columns = Math.floor(window.innerWidth / 10); // Number of columns based on window width
+  const snowflakes = [];
+
+  for (let i = 0; i < columns; i++) {
+    const snowflake = document.createElement("div");
+    snowflake.classList.add("matrix-text");
+    snowflake.innerText = getRandomSnowflake();
+    snowflake.style.left = i * 10 + "px"; // Position each column
+    snowflake.style.top = Math.floor(Math.random() * -1000) + "px"; // Randomize starting position
+    snowflakes.push(snowflake);
+    matrixContainer.appendChild(snowflake);
+  }
+
+  animateSnowflakes(snowflakes);
+}
+
+function animateSnowflakes(snowflakes) {
+  const speed = 50; // Adjust the speed of falling snowflakes
+  setInterval(() => {
+    for (let i = 0; i < snowflakes.length; i++) {
+      const snowflake = snowflakes[i];
+      let top = parseInt(snowflake.style.top);
+      if (top >= window.innerHeight) {
+        snowflake.style.top = Math.floor(Math.random() * -1000) + "px"; // Reset position when snowflake reaches the bottom
+      } else {
+        snowflake.style.top = top + 5 + "px"; // Increment the position to simulate falling snowflakes
+      }
     }
-  }
+  }, speed);
+}
 
-  // Check for previously set theme and apply it
+function getRandomSnowflake() {
+  const snowflakeSet = "❄️⛄";
+  return snowflakeSet[Math.floor(Math.random() * snowflakeSet.length)];
+}
+
+// function for checking the season and show the matrix
+function seasonalMatrix() {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1;
+  // Check if it's winter (assuming winter is December, January, February)
+  if (currentMonth > 12) {
+    //if (currentMonth >= 12 || currentMonth <= 2) {
+    createSnowflakes();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
   if (localStorage.getItem("theme") === "theme-dark") {
     setTheme("theme-dark");
   } else {
     setTheme("theme-light");
   }
 
-  const matrixContainer = document.querySelector(".matrix-container");
-
-  function createMatrix() {
-    const columns = Math.floor(window.innerWidth / 10); // Number of columns based on window width
-    const codeStreams = [];
-
-    for (let i = 0; i < columns; i++) {
-      const codeStream = document.createElement("div");
-      codeStream.classList.add("matrix-text");
-      codeStream.innerText = getRandomChar();
-      codeStream.style.left = i * 10 + "px"; // Position each column
-      codeStream.style.top = Math.floor(Math.random() * -1000) + "px"; // Randomize starting position
-      codeStreams.push(codeStream);
-      matrixContainer.appendChild(codeStream);
-    }
-
-    animateMatrix(codeStreams);
-  }
-
-  function animateMatrix(codeStreams) {
-    const speed = 50; // Adjust the speed of falling text
-
-    setInterval(() => {
-      for (let i = 0; i < codeStreams.length; i++) {
-        const codeStream = codeStreams[i];
-        let top = parseInt(codeStream.style.top);
-
-        if (top >= window.innerHeight) {
-          codeStream.style.top = Math.floor(Math.random() * -1000) + "px"; // Reset position when text reaches the bottom
-        } else {
-          codeStream.style.top = top + 5 + "px"; // Increment the position to simulate falling text
-        }
-      }
-    }, speed);
-  }
-
-  function getRandomChar() {
-    const charSet = "❄️⛄";
-    return charSet[Math.floor(Math.random() * charSet.length)];
-  }
-
-  createMatrix();
+  seasonalMatrix();
 });
