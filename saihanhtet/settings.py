76019ae7f7dbd957d4f9250e0e015c9660d7209a
saihanhtet <sai.hanhtetsan@gmail.com>
2024-01-01 20:31:54 +0700

    change the db and requirements

--- saihanhtet/settings.py
+++ saihanhtet/settings.py
@@ -79,9 +79,24 @@ WSGI_APPLICATION = 'saihanhtet.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': os.getenv('PGDATABASE'),
+
+        'USER': os.getenv('PGUSER'),
+
+        'PASSWORD': os.getenv('PGPASSWORD'),
+
+        'HOST': os.getenv('PGHOST'),
+
+        'PORT': os.getenv('PGPORT'),
+
     }
 }
 
